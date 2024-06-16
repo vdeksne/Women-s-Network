b8c794c52d319866f6c16f160fd97f711b162bfb
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2024-06-16 16:09:00 +0300

    homePage

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="blog-card">
-    <div v-show="editPost" class="icons">
-      <div @click="editBlog" class="icon">
+    <div class="icons">
+      <div class="icon">
         <Edit class="edit" />
       </div>
-      <div @click="deletePost" class="icon">
+
+      <div class="icon">
         <Delete class="delete" />
       </div>
     </div>
+
     <img
       :src="require(`../assets/blogCards/${post.blogCoverPhoto}.jpg`)"
       alt=""
     />
+
     <div class="info">
       <h4>{{ post.blogTitle }}</h4>
+
       <h6>Posted on: {{ post.blogDate }}</h6>
+
       <router-link class="link" to="#">
-        View the Post <Arrow class="arrow" />
+        View the Post
+        <Arrow class="arrow" />
       </router-link>
     </div>
   </div>
@@ -33,22 +39,6 @@ export default {
     Arrow,
     Edit,
     Delete,
-  },
-  methods: {
-    deletePost() {
-      this.$store.dispatch("deletePost", this.post.blogID);
-    },
-    editBlog() {
-      this.$router.push({
-        name: "EditBlog",
-        params: { blogid: this.post.blogID },
-      });
-    },
-  },
-  computed: {
-    editPost() {
-      return this.$store.state.editPost;
-    },
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,7 @@
       :key="index"
       :post="post"
     />
+
     <div class="blog-card-wrap">
       <div class="container">
         <h3>View recent blogs</h3>
@@ -18,15 +19,26 @@
         </div>
       </div>
     </div>
+
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss a post, register today!</h2>
+        <router-link class="router-button" to="#">
+          Register Now <Arrow class="arrow arrow-light" />
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "@/components/BlogPost.vue";
 import BlogCard from "@/components/BlogCard.vue";
+import Arrow from "@/assets/Icons/arrow-right-light.svg";
+
 export default {
   name: "Home",
-  components: { BlogPost, BlogCard },
+  components: { BlogPost, BlogCard, Arrow },
   data() {
     return {
       welcomeScreen: {
@@ -81,6 +93,39 @@ export default {
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 48px;
+      }
+    }
   }
 }
 </style>
